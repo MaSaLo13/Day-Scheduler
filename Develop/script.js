173f69451fd37e43a8e418753c9b46e6f91d391e
Marvin   Santiago <marvinsantiago@MindYouBusiness.lan>
2022-06-07 21:22:16 -0700

    text input

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
-//var hour1 = document.getElementById('hour');
-
-
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 
-// move id in colorcode to edit_cont
 var currentHour = (new Date()).getHours();
 $('.edit_cont')
   .each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 24){
+      $(this).css('background-color','Blue');
+    }else if(val < currentHour && val > currentHour-24){
+      $(this).css('background-color','Red');
+    }else if(val === currentHour){
+      $(this).css('background-color','Green');
+    }else{
+      $(this).css('background-color','White');
+    }
+  });
+
+  
+  /*setInterval(function(){ 
+    //this code runs every second 
+}, 1000);
+
+
+
+/*  .each(function(){
     var val = parseInt($(this).prop('id'));
     if(val > currentHour && val < currentHour + 6){
       $(this).css('background-color','Blue');
@@ -20,19 +36,6 @@ $('.edit_cont')
       $(this).css('background-color','White');
     }
   });
-
-  
-
-
-
-/*function colorChange() {
-    var currentTime = moment().format("h:mm:ss a");
-    if (currentTime === '9 am') {
-        document.getElementById("hour").className = "past";
-    }
-   
-}*/
-
 
 
 /*Function for updating the hour colors gray is past, red is now, green is future 
